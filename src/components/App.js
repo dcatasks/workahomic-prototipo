1e2dd6626adf7622c51bd1746a8b7837ab0a8ba0
Daniel <dcatalan.intern@das-nano.com>
2020-12-13 18:20:22 +0100

    firs commit prototipo san valero

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import TaskList from './TaskList';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Hola</h2>
+        <TaskList></TaskList>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
---
+++ src/components/TaskList.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const TaskList = ({ title, cards, listID, index, dispatch }) => {
+    return(
+        <h3>List</h3>
+    )
+}
+
+export default TaskList;
